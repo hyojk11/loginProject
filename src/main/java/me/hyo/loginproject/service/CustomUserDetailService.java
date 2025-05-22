2ecec1719ca0364d54f76dae235ef577afbff055
hyojk11 <hyojk11@gmail.com>
2025-05-22 19:40:10 +0900

    feat spring security setting

---
+++ src/main/java/me/hyo/loginproject/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package me.hyo.loginproject.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hyo.loginproject.domain.User;
+import me.hyo.loginproject.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
